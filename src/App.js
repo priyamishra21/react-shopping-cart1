a6147a9961d5ae83b8d7baeedab8ded14bb5a005
priya <pm10236@gmail.com>
2021-06-04 10:13:56 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './index.css'
+class App extends React.Component{
+    render()
+    {
+        return(
+<div className="grid-container">
+    <header>
+        <a href='/'>React Shopping cart</a>
+    </header>
+    <main>
+<p>product list</p>
+    </main>
+    <footer>
+        All right is reserved
+    </footer>
+</div>
+        
+        );
+    }
+}
+
+  
+export default App;
